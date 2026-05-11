56f4a3d92298d1755117850a7fb2310fe0a19d27
Rosli Zaqwan <66868007+Hantu-Raya@users.noreply.github.com>
2026-05-11 19:25:56 +0800

    Fix GitHub Pages deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://hantu-raya.github.io",
-  base: process.env.GITHUB_PAGES ? "/color-blind-web-builder" : undefined,
+  base: process.env.GITHUB_PAGES ? "/color-blind-web-builder/" : undefined,
   integrations: [react()]
 });
